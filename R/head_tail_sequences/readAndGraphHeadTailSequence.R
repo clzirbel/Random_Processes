3866146cd56ddd6acc28fd519b224367563aaead
Craig L. Zirbel <zirbel@bgsu.edu>
2017-01-17 09:20:33 -0500

    Graphs of HT sequences as up down walks

---
+++ R/head_tail_sequences/readAndGraphHeadTailSequence.R
@@ -0,0 +1,26 @@
+# This program displays a sequences of H and T as up and down steps
+
+sequencenumber = "03"       # for sequences 01 to 16
+sequencenumber = "IID"      # for the sequence of IID H and T
+
+T = read.table(paste("https://raw.githubusercontent.com/clzirbel/Random_Processes/master/R/head_tail_sequences/sequence",sequencenumber,".txt",sep=""),stringsAsFactors=FALSE)
+S = T[1,1]
+
+t = 0:nchar(S)                   # time points
+X = rep.int(0,nchar(S))          # location of the head-tail process
+
+for (i in 1:nchar(S)) {
+  if (substr(S,i,i) == 'H') {
+    X[i+1] = X[i] + 1
+  } else {
+    X[i+1] = X[i] - 1
+  }
+}
+
+a = 0
+b = 1000
+c = min(X[(a+1):(b+1)])
+d = max(X[(a+1):(b+1)])
+
+plot(t,X,type="l",xlim=c(a,b),ylim=c(c,d),xlab="Step number",ylab="Position",main=paste("Head-tail sequence ",sequencenumber," as an up down walk",sep=""))
+
